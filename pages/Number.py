5379a434e650b748fbaec288b524819caddafc8b
ankushsaini2145-hash <236213137+ankushsaini2145-hash@users.noreply.github.com>
2026-06-21 13:03:10 +0000

    Updated project

--- pages/Number.py
+++ pages/Number.py
@@ -3,31 +3,38 @@ import random as r
 c1,c2=st.columns([1,3])
 with c2:
     st.markdown("## Number Gassing Game ##")
+if 'Score' not in st.session_state:
+    st.session_state['Score']=0
 def Game(user):
     if st.button("Submit"):
         if st.session_state['i']==user:
-            st.write("Correct Number")
+            st.success("Correct Number")
+            st.session_state['Score']+=1
             if st.button("Next"):
+                st.session_state['i']=Number()
                 st.rerun()
         else:
-            st.write("Wrong")
-c3,c4,c5=st.columns([.5,3,.5])
-with c4:
-    Level=st.selectbox("Select Your Level",["beginner","Midium","Pro"])
-if 'a' not in st.session_state:
-    st.session_state['a']=Level
-st.write(st.session_state['a'])
-if st.session_state['a']=="beginner":
-    def Number():
-        a=r.randrange(1,11)
-        return a
-    if 'i' not in st.session_state:
-            st.session_state['i']=Number()
-    user=st.number_input("Enter Choice Number(1 to 10)",min_value=0)
-    Game(user)
-elif st.session_state['a']=="Midium":
-    def Number():
-        a=r.randrange(1,51)
-        return a
-    if 'i' not in st.session_state:
-        pass
+            st.write("Hint")
+            if st.session_state['i']>user:
+                st.write("Number is Big")
+            elif st.session_state['i']<user:
+                st.write("Number Is Less")
+c6,c7=st.columns(2)
+def Number():
+    a=r.randrange(1,51)
+    return a
+if 'i' not in st.session_state:
+    st.session_state['i']=Number()
+user=st.number_input("Enter Choice Number(1 to 50)",min_value=0)
+if st.button("Submit",key=1):
+    if st.session_state['i']==user:
+        st.write("Correct Number")
+        if st.button("Next",key=1):
+            st.rerun()
+    else:
+        st.write("Wrong")
+Game(user)
+with c6:
+    pass
+with c7:
+    st.write("Your Score",st.session_state['Score'])
