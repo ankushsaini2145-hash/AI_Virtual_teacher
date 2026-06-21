f0fa6a28bb2b5af16f55acf8abce63925ba7194f
ankushsaini2145-hash <236213137+ankushsaini2145-hash@users.noreply.github.com>
2026-06-21 13:57:46 +0000

    Updated project

---
+++ pages/Expense.py
@@ -0,0 +1,33 @@
+import streamlit as st
+if 'i' not in st.session_state:
+    st.session_state['i']=[]
+if 'Sum' not in st.session_state:
+    st.session_state['Sum']=0
+c1,c2=st.columns([1,3])
+with c1:
+    pass
+with c2:
+    st.markdown("## Expense Tracker ##")
+with st.form("Form"):
+        user=st.text_input("Enter Name:")
+        category=st.text_input("Enter Expense Category (Food, Shopping, ext.):")
+        date=st.date_input("Enter Expense Date:")
+        description=st.text_input("Enter Description:")
+        Amount=st.number_input("Enter Ammount for course:",min_value=0)
+        if st.form_submit_button("Submit"):
+            S={
+                "User":user,
+                "Category":category,
+                "Date":date,
+                "Description":description,
+                "Amount":Amount
+                }
+            st.session_state['i'].append(S)
+            st.session_state['Sum']+=Amount
+            for a in st.session_state['i']:
+                st.write(a["User"])
+                st.write("Expense Category",a["Category"])
+                st.write(a["Date"])
+                st.write(a["Description"])
+                st.write(a["Amount"])
+                st.write("Total Expense Amount is:",st.session_state['Sum'])
--- pages/Number.py
+++ pages/Number.py
@@ -5,20 +5,6 @@ with c2:
     st.markdown("## Number Gassing Game ##")
 if 'Score' not in st.session_state:
     st.session_state['Score']=0
-def Game(user):
-    if st.button("Submit"):
-        if st.session_state['i']==user:
-            st.success("Correct Number")
-            st.session_state['Score']+=1
-            if st.button("Next"):
-                st.session_state['i']=Number()
-                st.rerun()
-        else:
-            st.write("Hint")
-            if st.session_state['i']>user:
-                st.write("Number is Big")
-            elif st.session_state['i']<user:
-                st.write("Number Is Less")
 c6,c7=st.columns(2)
 def Number():
     a=r.randrange(1,51)
@@ -26,14 +12,18 @@ def Number():
 if 'i' not in st.session_state:
     st.session_state['i']=Number()
 user=st.number_input("Enter Choice Number(1 to 50)",min_value=0)
-if st.button("Submit",key=1):
+if st.button("Submit"):
     if st.session_state['i']==user:
-        st.write("Correct Number")
-        if st.button("Next",key=1):
-            st.rerun()
-    else:
-        st.write("Wrong")
-Game(user)
+        st.success("Correct Number")
+        st.session_state['Score']+=1
+if st.button("Hint"):
+        if st.session_state['i']>user:
+            st.write("Number Is Big")
+        elif st.session_state['i']<user:
+            st.write("Number Is Less")
+if st.button("Next"):
+    st.session_state['i']=Number()
+    st.rerun()
 with c6:
     pass
 with c7:
